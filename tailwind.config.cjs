08e2e455fe6a537f2a79f19668aeaff658843668
37108 <37108mail@gmail.com>
2023-04-18 08:37:09 +0900

    feat: update config

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    fontFamily: {
+      'rale-way': ['Raleway', 'sans-serif'],
+      'noto-sans': ['Noto Sans JP', 'sans-serif']
+    },
+    extend: {
+      spacing: [...Array(100 - 3)]
+        .map((_, i) => i + 4)
+        .reduce(
+          (acc, cur) => ({
+            ...acc,
+            [cur]: String(cur * 0.25) + 'rem',
+          }),
+          {},
+        )},
+	},
+}

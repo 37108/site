94cd83ed4d0f23864141102db7f1b0e40f1f737c
37108 <37108mail@gmail.com>
2023-04-18 14:13:35 +0900

    feat: add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://37108.dev',
   integrations: [tailwind(), mdx()],
   markdown: {
     shikiConfig: {

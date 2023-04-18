405c1cb54abcb663c5917e6086f8a4fde6da6e52
37108 <37108mail@gmail.com>
2023-04-18 21:27:27 +0900

    fix: update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://37108.dev',
-  integrations: [tailwind(), mdx()],
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: "dark-plus",
-    },
+      theme: "dark-plus"
+    }
   }
 });

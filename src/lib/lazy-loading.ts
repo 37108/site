01af93993d9233acff177743c8997f52c80816f7
37108 <37108mail@gmail.com>
2024-03-28 10:57:27 +0900

    modify plugin

--- src/lib/lazy-loading.ts
+++ src/lib/lazy-loading.ts
@@ -1,15 +1,27 @@
 import type { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 import type { Element } from "hast";
+import sizeOf from "image-size";
 
 export const rehypeLazy: Plugin = () => {
   return (tree) => {
     visit(tree, "element", (element: Element) => {
       if (element.tagName === "img") {
+        let sizes: { width?: number | undefined; height?: number | undefined } =
+          {};
+        const src = element.properties.src;
+        if (src && typeof src === "string" && src.startsWith("/")) {
+          const res = sizeOf("./public" + element.properties.src);
+          sizes.width = res.width;
+          sizes.height = res.height;
+        }
+
+        const res = sizeOf(("./public" + element.properties.src) as string);
         element.properties = {
           ...(element.properties || {}),
           loading: "lazy",
           decoding: "async",
+          ...sizes,
         };
       }
     });

0fcae8048fb7d7bf2335a24ad55f736b69a8296a
37108 <37108mail@gmail.com>
2025-09-07 04:46:46 +0900

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,33 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { rehypeLazy } from "./src/lib/lazy-loading";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://37108.dev",
   output: "static",
+
   integrations: [
     partytown({
       config: {
         forward: ["dataLayer.push"],
       },
     }),
-    tailwind(),
     mdx(),
     sitemap(),
   ],
+
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
     },
     rehypePlugins: [rehypeLazy],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
